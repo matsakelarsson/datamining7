30cac2c413d9666888d8a0a3a50013e8806930b3
Mats Larsson <m.larsson88@gmail.com>
2019-10-07 15:06:53 +0200

    sandbox

--- src/fpldata.py
+++ src/fpldata.py
@@ -6,6 +6,17 @@ import os
 import pandas as pd
 import csv
 
-print(os.listdir("../fpl-data")) # Prints all csv files
-df = pd.read_csv("../fpl-data/FPL_2018_19_Wk0.csv") # Loads on csv file
-print(df.head()) # Prints the head of the loaded csv-file
+#print(os.listdir("../fpl-data")) # Prints all csv files
+df = pd.read_csv("../data/2019-20/gws/gw8.csv") # Loads on csv file
+#print(df.head()) # Prints the head of the loaded csv-file
+df.index += 1
+df['100 adj'] = df['total_points'].rolling(window=100).mean()
+print(df.head())
+print(df.index)
+ax1 = plt.subplot2grid((6,1), (0, 0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5, 0), rowspan=1, colspan=1,)
+ax1.scatter(df['selected'], df['total_points'])
+ax1.scatter(df['selcted'], df[''])
+ax2.bar(df['minutes'], df['GW'])
+
+plt.show()
